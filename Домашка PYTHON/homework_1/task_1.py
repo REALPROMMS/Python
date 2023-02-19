69b7570e03c487d67097455a949ae8d43c572e4a
REALPROMMS <m.makarov90@bk.ru>
2023-02-19 12:07:14 +0300

    done task

--- Домашка PYTHON/homework_1/task_1.py
+++ Домашка PYTHON/homework_1/task_1.py
@@ -7,3 +7,11 @@ sum = int(str(random_number)[0]) + int(str(random_number)[1]) + int(str(random_n
 print(f"Сумма каждого эелмента числа {random_number}, равняется = {sum}")
 
 
+# второй вариант
+
+# random_number = int(input("Введите любое трёхзначное число: "))
+# print(random_number)
+
+# sum = int(str(random_number)[0]) + int(str(random_number)[1]) + int(str(random_number)[2])
+
+# print(f"Сумма каждого эелмента числа {random_number}, равняется = {sum}")

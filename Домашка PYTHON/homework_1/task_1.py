b3d4ae8a8d310c9be414869620e72ef1a77cd975
REALPROMMS <m.makarov90@bk.ru>
2023-02-19 12:03:18 +0300

    done task

---
+++ Домашка PYTHON/homework_1/task_1.py
@@ -0,0 +1,9 @@
+from random import randint
+
+random_number = randint(100, 999)
+print(random_number)
+
+sum = int(str(random_number)[0]) + int(str(random_number)[1]) + int(str(random_number)[2])
+print(f"Сумма каждого эелмента числа {random_number}, равняется = {sum}")
+
+

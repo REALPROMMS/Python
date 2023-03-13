6fea960aa19de30b770dcede07aaac40e8d13be5
REALPROMMS <m.makarov90@bk.ru>
2023-03-13 10:02:41 +0300

    done

---
+++ Лекции PYTHON/task_4.py
@@ -0,0 +1,17 @@
+
+my_list = [1, 2, 3, 5, 8, 15, 23, 38]
+
+new_list = [
+    
+]
+for i in my_list:
+    if i % 2 == 0:
+        new_list.append((i, i**2))
+        
+print(new_list) 
+
+
+
+
+
+   	
